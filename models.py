5220351b968ead9347aecb4474d08ff4b617745a
LCY000 <zqwe1230@gmail.com>
2024-12-18 20:21:18 +0800

    "我的密碼"功能v1 (複製功能)

--- models.py
+++ models.py
@@ -1,6 +1,21 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#加載加密密鑰
+encryption_key = os.getenv('ENCRYPTION_KEY')
+if encryption_key is None:
+    raise ValueError("ENCRYPTION_KEY未定義！")
+
+try:
+    cipher = Fernet(encryption_key.encode())
+except ValueError:
+    raise ValueError("提供的 ENCRYPTION_KEY 無效！")
 
 db = SQLAlchemy()
 
@@ -17,6 +32,15 @@ class PasswordEntry(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     site_name = db.Column(db.String(150), nullable=False)
     site_username = db.Column(db.String(150), nullable=False)
-    site_password = db.Column(db.String(300), nullable=False)  # 儲存經加鹽哈希過的密碼
+    site_password = db.Column(db.String(300), nullable=False)  # 加密後存儲
     strength_score = db.Column(db.Integer, default=0)  # 預設強度分數為0
     user = db.relationship('User', backref='password_entries', lazy=True)
+
+    # 新增方法解密密碼
+    def get_plaintext_password(self):
+        return cipher.decrypt(self.site_password.encode()).decode()
+
+    # 新增方法加密密碼
+    @staticmethod
+    def set_encrypted_password(password):
+        return cipher.encrypt(password.encode()).decode()
