5fb538c0fc6036c2395d3fdd51dbe785c0ba0243
LCY000 <zqwe1230@gmail.com>
2024-12-18 17:17:52 +0800

    base.html修改登出按鈕bug

--- utils/password_manager.py
+++ utils/password_manager.py
@@ -1,4 +1,5 @@
 # password_manager.py
+
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from models import db, PasswordEntry
