a89ffad0d01315d54d9933f2b9cbe130b4b3eb60
LCY000 <zqwe1230@gmail.com>
2024-12-18 23:18:55 +0800

    merge

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)  # email
+    email = db.Column(db.String(120), unique=True, nullable=False)  # 新增 email 欄位
     salt = db.Column(db.String(32), nullable=False)  # 專屬鹽值
 
 # 新增的密碼儲存 Model
